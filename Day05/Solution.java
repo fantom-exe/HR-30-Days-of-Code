66f5c684062a0c52801d45e9dc8084b4d8d890d7
Edward <eisajanyan@gmail.com>
2018-05-04 02:19:53 -0700

    finished day 5

---
+++ Day05/Solution.java
@@ -0,0 +1,20 @@
+package Day05;
+
+import java.util.*;
+
+public class Solution {
+	
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		
+		// solution
+		int i;
+		for(i = 1; i <= 10; i++) {
+			System.out.println(n + " x " + i + " = " + (n * i));
+		}
+		
+	}
+	
+}
+
