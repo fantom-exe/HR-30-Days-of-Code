bd00bcadde112f4426a28a1878f02c4367c03998
Edward <eisajanyan@gmail.com>
2018-05-04 01:50:05 -0700

    added already completed challenges

---
+++ Day02/Solution.java
@@ -0,0 +1,18 @@
+package Day02;
+
+import java.util.*;
+
+public class Solution {
+	
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		double meal_cost = in.nextDouble();
+		int tip_percent = in.nextInt();
+		int tax_percent = in.nextInt();
+		in.close();
+		
+		double totalCost = meal_cost + (meal_cost * tip_percent/100) + (meal_cost * tax_percent/100);
+		System.out.println("The total meal cost is " + Math.round(totalCost) + " dollars.");
+	}
+	
+}
