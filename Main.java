39250da940e5b93e6c89c15fc953c637be91c3bd
Edward <eisajanyan@gmail.com>
2018-05-13 23:59:17 -0700

    day 9 in progress

--- Main.java
+++ Main.java
@@ -1,9 +1,11 @@
-import Day08
+import Day09
 		.*;
+
+import java.io.IOException;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		String[] strArgs = new String[0];
 		
 		Solution.main(strArgs);
