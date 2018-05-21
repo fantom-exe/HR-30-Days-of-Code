4e965dbb233a2267a2ddb3980414f1cbf88ad05a
Ed <eisajanyan@gmail.com>
2018-05-21 01:37:54 -0700

    day 13 started

---
+++ Day13/MyBook.java
@@ -0,0 +1,23 @@
+package Day13;
+
+
+class MyBook {
+	// Declare the price instance variable
+	
+	/**
+	 *   Class Constructor
+	 *
+	 *   @param title The book's title.
+	 *   @param author The book's author.
+	 *   @param price The book's price.
+	 **/
+	// Write your constructor here
+	
+	/**
+	 *   Method Name: display
+	 *
+	 *   Print the title, author, and price in the specified format.
+	 **/
+	// Write your method here
+	
+}
