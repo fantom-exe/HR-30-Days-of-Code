3dd03d540a6052c2f2bd5e3472413000166367be
Ed <eisajanyan@gmail.com>
2018-05-21 14:00:32 -0700

    day 15 started

---
+++ Day15/Solution.java
@@ -0,0 +1,41 @@
+package Day15;
+
+import java.util.Scanner;
+
+class Node {
+	int data;
+	Node next;
+	Node(int d) {
+		data = d;
+		next = null;
+	}
+}
+
+public class Solution {
+	
+	public static Node insert(Node head, int data) {
+		//Complete this method
+	}
+	
+	public static void display(Node head) {
+		Node start = head;
+		while(start != null) {
+			System.out.print(start.data + " ");
+			start = start.next;
+		}
+	}
+	
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		Node head = null;
+		int N = sc.nextInt();
+		
+		while(N-- > 0) {
+			int ele = sc.nextInt();
+			head = insert(head,ele);
+		}
+		display(head);
+		sc.close();
+	}
+	
+}
