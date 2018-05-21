efaffe724760206c4f87ae0f79903cadbf950e7b
Ed <eisajanyan@gmail.com>
2018-05-21 01:31:01 -0700

    day 12 complete

--- Day12/Solution.java
+++ Day12/Solution.java
@@ -2,7 +2,7 @@ package Day12;
 
 import java.util.Scanner;
 
-class Solution {
+public class Solution {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		String firstName = scan.next();
