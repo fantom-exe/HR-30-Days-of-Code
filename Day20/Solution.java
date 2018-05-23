717e38f6f6505ad4f129b6c936b57ac48153fcef
Edward <eisajanyan@gmail.com>
2018-05-23 02:10:24 -0700

    day 20 complete

--- Day20/Solution.java
+++ Day20/Solution.java
@@ -11,6 +11,48 @@ public class Solution {
 		for(int a_i=0; a_i < n; a_i++){
 			a[a_i] = in.nextInt();
 		}
-		// Write Your Code Here
+		
+		// output vars
+		int numSwaps = 0;   // 1.
+		int firstElement;   // 2.
+		int lastElement;    // 3.
+		
+		// sort
+		for (int i = 0; i < n; i++) {
+			// Track number of elements swapped during a single array traversal
+			int numberOfSwaps = 0;
+			int tmp;
+			
+			for (int j = 0; j < n - 1; j++) {
+				// Swap adjacent elements if they are in decreasing order
+				if (a[j] > a[j + 1]) {
+					tmp = a[j];
+					a[j] = a[j+1];
+					a[j+1] = tmp;
+					
+					numberOfSwaps++;
+				}
+			}
+			
+			numSwaps += numberOfSwaps;
+			
+			// If no elements were swapped during a traversal, array is sorted
+			if (numberOfSwaps == 0) {
+				break;
+			}
+		}
+		
+	// 1.
+		System.out.println("Array is sorted in " + numSwaps + " swaps.");
+		
+	// 2.
+		firstElement = a[0];
+		System.out.println("First Element: " + firstElement);
+		
+	// 3.
+		lastElement = a[a.length-1];
+		System.out.println("Last Element: " + lastElement);
+	
 	}
+	
 }
