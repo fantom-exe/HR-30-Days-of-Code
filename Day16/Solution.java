63fb86c483ae9d23db06b193acbf68b11e5df349
Edward <eisajanyan@gmail.com>
2018-05-23 00:39:05 -0700

    day 16 complete

--- Day16/Solution.java
+++ Day16/Solution.java
@@ -7,6 +7,13 @@ public class Solution {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		String S = in.next();
+		
+		try {
+			int num = Integer.parseInt(S);
+			System.out.println(num);
+		} catch(NumberFormatException e) {
+			System.out.println("Bad String");
+		}
 	}
 	
 }
