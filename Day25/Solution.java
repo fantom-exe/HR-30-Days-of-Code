68a0f43c627e5e583c8cb42fca21031953ce4fe8
Edward <eisajanyan@gmail.com>
2018-05-28 04:04:39 -0700

    day 25 ip

--- Day25/Solution.java
+++ Day25/Solution.java
@@ -5,10 +5,36 @@ import java.util.Scanner;
 public class Solution {
 	
 	public static void main(String[] args) {
-		/* Read input from STDIN. Print output to STDOUT. */
 		Scanner input = new Scanner(System.in);
 		
+		int runs = input.nextInt();
 		
+		while(runs > 0) {
+			int num = input.nextInt();
+			boolean prime = true;
+			
+			if(num % 2 == 0) { // even number
+				prime = false;
+			}
+			else {
+				for(int i = num / 2; i > 1; i--) {
+					if(num % i == 0) {
+						prime = false;
+						break;
+					}
+				}
+			}
+			
+			// output
+			if(prime) {
+				System.out.println("Prime");
+			}
+			else {
+				System.out.println("Not prime");
+			}
+			
+			runs--;
+		}
 		
 	}
 	
