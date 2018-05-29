952beb509e8309f4c16a8d939e4efe9f76a92fa3
Ed <eisajanyan@gmail.com>
2018-05-29 14:06:09 -0700

    day 26 ip

--- Day26/Solution.java
+++ Day26/Solution.java
@@ -7,14 +7,31 @@ public class Solution {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		
-		int[] dueDate    = new int[3];
 		int[] actualDate = new int[3];
+		int[] dueDate    = new int[3];
 		
 		// input
 		for (int i = 0; i < 6; i++) {
 			int num = input.nextInt();
 			
+			if(i < 3) {
+				actualDate[i] = num;
+			}
+			else {
+				dueDate[i-3] = num;
+			}
+		}
+		
+		int fine = 0;
+		// calculate
+		for (int i = 2; i >= 0; i--) {
 			
+			if(actualDate[i] <= dueDate[i]) {
+				fine = 0;
+			}
+			else if () {
+				
+			}
 		}
 		
 	}
