0986f19346d52c85dc8a9daa19ebab16bede04f0
Ed <eisajanyan@gmail.com>
2018-05-29 13:51:36 -0700

    day 26 started

---
+++ Day26/Solution.java
@@ -0,0 +1,22 @@
+package Day26;
+
+import java.util.Scanner;
+
+public class Solution {
+	
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		int[] dueDate    = new int[3];
+		int[] actualDate = new int[3];
+		
+		// input
+		for (int i = 0; i < 6; i++) {
+			int num = input.nextInt();
+			
+			
+		}
+		
+	}
+	
+}
