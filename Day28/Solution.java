e2f32b7cf31c2c380966c2f0f210d22c5083a2bc
Edward <eisajanyan@gmail.com>
2018-06-14 04:01:14 -0700

    day 28 ip

--- Day28/Solution.java
+++ Day28/Solution.java
@@ -1,10 +1,13 @@
 package Day28;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Solution {
-	
-	
 	
 	private static final Scanner scanner = new Scanner(System.in);
 	
@@ -12,12 +15,28 @@ public class Solution {
 		int N = scanner.nextInt();
 		scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 		
+		Pattern p = Pattern.compile(".+@gmail\\.com$");
+		List<String> sortedNames = new ArrayList<>();
 		for (int NItr = 0; NItr < N; NItr++) {
 			String[] firstNameEmailID = scanner.nextLine().split(" ");
 			
 			String firstName = firstNameEmailID[0];
 			
 			String emailID = firstNameEmailID[1];
+			
+			Matcher m = p.matcher(emailID);
+			
+			if(m.find()) {
+				sortedNames.add(firstName);
+			}
+		}
+		
+		// sort
+		Collections.sort(sortedNames);
+		
+		// print
+		for(int i = 0; i < sortedNames.size(); i++) {
+			System.out.println(sortedNames.get(i));
 		}
 		
 		scanner.close();
